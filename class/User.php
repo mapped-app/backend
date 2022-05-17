b0ac09fc70b0426d991e4b15bf11dbf3852c2b03
Hassan <ahassan0510@icloud.com>
2022-05-17 17:14:49 +0200

    code cleanup and reformat in User class

--- class/User.php
+++ class/User.php
@@ -2,34 +2,32 @@
 
 class User
 {
-    // Connection
     private $conn;
-    // Table
     private $db_table = "users";
-    // Columns
     public $id;
     public $name;
     public $email;
     public $password;
     public $phone;
     public $is_active;
-    public $age;
-    public $designation;
     public $created;
-    // Db connection
-    public function __construct($db){
+
+    public function __construct($db)
+    {
         $this->conn = $db;
     }
-    // GET ALL
-    public function getUsers(){
+
+    public function getUsers()
+    {
         $sqlQuery = "SELECT * FROM " . $this->db_table . "";
         $stmt = $this->conn->prepare($sqlQuery);
         $stmt->execute();
         return $stmt;
     }
-    // CREATE
-    public function createUser(){
-        $sqlQuery = "INSERT INTO". $this->db_table ."SET name = :name, email = :email, password = :password, phone = :phone, created = :created, is_active = :is_active";
+
+    public function createUser()
+    {
+        $sqlQuery = "INSERT INTO" . $this->db_table . "SET name = :name, email = :email, password = :password, phone = :phone, created = :created, is_active = :is_active";
 
         $stmt = $this->conn->prepare($sqlQuery);
 
@@ -49,14 +47,15 @@ class User
         $stmt->bindParam(":created", $this->created);
         $stmt->bindParam(":is_active", $this->is_active);
 
-        if($stmt->execute()){
+        if ($stmt->execute()) {
             return true;
         }
         return false;
     }
-    // READ single
-    public function getSingleUser(){
-        $sqlQuery = "SELECT * FROM". $this->db_table ."WHERE id = ? LIMIT 0,1";
+
+    public function getSingleUser()
+    {
+        $sqlQuery = "SELECT * FROM" . $this->db_table . "WHERE id = ? LIMIT 0,1";
 
         $stmt = $this->conn->prepare($sqlQuery);
         $stmt->bindParam(1, $this->id);
@@ -70,10 +69,11 @@ class User
         $this->created = $dataRow['created'];
         $this->is_active = $dataRow['is_active'];
     }
-    // UPDATE
-    public function updateUser(){
+
+    public function updateUser()
+    {
         $sqlQuery = "UPDATE
-                        ". $this->db_table ."
+                        " . $this->db_table . "
                     SET
                         name = :name,
                         email = :email,
@@ -104,21 +104,22 @@ class User
         $stmt->bindParam(":is_active", $this->is_active);
         $stmt->bindParam(":id", $this->id);
 
-        if($stmt->execute()){
+        if ($stmt->execute()) {
             return true;
         }
         return false;
     }
-    // DELETE
-    function deleteUser(){
+
+    function deleteUser()
+    {
         $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE id = ?";
         $stmt = $this->conn->prepare($sqlQuery);
 
-        $this->id=htmlspecialchars(strip_tags($this->id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
 
         $stmt->bindParam(1, $this->id);
 
-        if($stmt->execute()){
+        if ($stmt->execute()) {
             return true;
         }
         return false;
