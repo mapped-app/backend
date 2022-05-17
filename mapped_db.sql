1fdde9dee2250148c638a33a344f1bc99155dac0
Hassan <ahassan0510@icloud.com>
2022-05-17 16:33:43 +0200

    update users table

--- mapped_db.sql
+++ mapped_db.sql
@@ -62,7 +62,8 @@ CREATE TABLE IF NOT EXISTS `sites` (
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `phone` varchar(15) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
