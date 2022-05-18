2977c0cfaa0d6c16c9b9082715beb29f4dbfb3c7
Hassan <ahassan0510@icloud.com>
2022-05-18 17:50:07 +0200

    add password in users & remove auto increment

--- mapped_db.sql
+++ mapped_db.sql
@@ -61,9 +61,10 @@ CREATE TABLE IF NOT EXISTS `sites` (
 
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `phone` varchar(15) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
