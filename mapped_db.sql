0dbd7395ae8f8d8fb6aee1f8364a87ab4934f5a9
Hassan <ahassan0510@icloud.com>
2022-05-18 18:16:15 +0200

    rename mail 👉 email

--- mapped_db.sql
+++ mapped_db.sql
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `sites` (
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone` varchar(15) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
