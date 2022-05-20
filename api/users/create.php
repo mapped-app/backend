7f4ba007381548070b51047a9c4a9bfb78a9840e
Hassan <ahassan0510@icloud.com>
2022-05-20 18:45:22 +0200

    debug: add 200 response

--- api/users/create.php
+++ api/users/create.php
@@ -6,6 +6,8 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+http_response_code(200);
+
 include_once '../../config/database.php';
 include_once '../../class/User.php';
 
