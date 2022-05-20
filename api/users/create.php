1430ca5d71763a2181af88286de668a302cff517
Hassan <ahassan0510@icloud.com>
2022-05-20 18:54:46 +0200

    debug: add headers

--- api/users/create.php
+++ api/users/create.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Auth-Token, X-Requested-With");
 
 include_once '../../config/database.php';
 include_once '../../class/User.php';
