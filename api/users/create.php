af9a311de3059eb8b8cd54cb7da240f9d08f09af
Hassan <ahassan0510@icloud.com>
2022-05-20 19:06:12 +0200

    change users/create api to GET from POST

--- api/users/create.php
+++ api/users/create.php
@@ -2,9 +2,9 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Auth-Token, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../../config/database.php';
 include_once '../../class/User.php';
