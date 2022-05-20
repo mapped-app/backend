844d1f4b3e11b1bdc3854beaa419c60d6da78c1f
Hassan <ahassan0510@icloud.com>
2022-05-20 18:52:05 +0200

    debug: add OPTIONS header

--- api/users/create.php
+++ api/users/create.php
@@ -1,14 +1,10 @@
 <?php
 
-http_response_code(200);
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-http_response_code(200);
 
 include_once '../../config/database.php';
 include_once '../../class/User.php';
