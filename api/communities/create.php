52bc2cc501b2b9cfeb9dfefbd66a7a1f5e72505b
Hassan <ahassan0510@icloud.com>
2022-05-20 16:31:12 +0200

    add communities/create api

---
+++ api/communities/create.php
@@ -0,0 +1,32 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../../config/database.php';
+include_once '../../class/Community.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$item = new Community($db);
+$data = json_decode(file_get_contents("php://input"));
+
+$item->name = $data->name;
+
+if ($item->createCommunity()) {
+    http_response_code(200);
+
+    $item->getCommunityById();
+
+    echo json_encode(array(
+        "status" => "success",
+        "message" => "Community was created",
+        "community_id" => $item->community_id,
+        "name" => $item->name
+    ));
+} else {
+    echo 'Community could not be created';
+}
